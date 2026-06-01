3de37903ad5a6690af5bb6abe76b864cda34feed
AkshatVassra <akshatvassra456@gmail.com>
2026-06-01 14:15:41 +0530

    new4

--- api/index.ts
+++ api/index.ts
@@ -2,17 +2,9 @@ export const config = {
   runtime: "edge",
 };
 
-let serverPromise: Promise<any> | undefined;
-
-async function getServer() {
-  if (!serverPromise) {
-    serverPromise = import(new URL("../dist/client/server/server.js", import.meta.url));
-  }
-  return serverPromise;
-}
+import serverModule from "../dist/client/server/server.js";
 
 export default async function handler(request: Request) {
-  const serverModule = await getServer();
   const response = await serverModule.default.fetch(request);
   return response;
 }
